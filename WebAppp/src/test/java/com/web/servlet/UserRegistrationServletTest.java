7532ff83d7ca83eb889b687925b02e3b69ee970f
GorobetsDima <007dragon1988@gmail.com>
2015-12-22 23:18:19 +0200

    Start to do servlet tests

---
+++ WebAppp/src/test/java/com/web/servlet/UserRegistrationServletTest.java
@@ -0,0 +1,26 @@
+package com.web.servlet;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ */
+public class UserRegistrationServletTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testDoGet() throws Exception {
+
+    }
+
+    public void testDoPost() throws Exception {
+
+    }
+}
