f2a84fdd20779ae37e021fe3b404cc3ae3095cc5
GorobetsDima <007dragon1988@gmail.com>
2015-12-28 18:07:37 +0200

    Game class+GameTest class

---
+++ BowlingGame/src/main/java/com/gorobets/bowlingGame/GameRunner.java
@@ -0,0 +1,13 @@
+package com.gorobets.bowlingGame;
+
+/**
+ * Hello world!
+ *
+ */
+public class GameRunner
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
